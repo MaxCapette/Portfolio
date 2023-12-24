0b307e96589d49495f993894daa982b4600d2447
Dokuyaku <sk8baz@gmail.com>
2023-12-24 22:11:21 +0100

    about ok2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import './globals.css'
 import Title from '@/components/Title/Title'
 import Projects from '@/components/projects/Projects'
-import About from '@/components/about/about'
+import About from '@/components/about/About'
 export default function Home() {
   return (
     <main className="main flex min-h-screen flex-col items-center justify-between ">
