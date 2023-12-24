0f167bc418d82eea85e1eb94b824e6543a726364
Dokuyaku <sk8baz@gmail.com>
2023-12-24 18:58:43 +0100

    second commit responsive

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -7,16 +7,17 @@ export default function Projects() {
     if (projects.length === 0) {
         return <div className="projectContainer">No projects available.</div>;
       }
+      const reversedProjects = [...projects].reverse();
   return (
     <div className="projectContainer" >
       <h2>Projects</h2>
       <p className="projectsIntro">Here are different projects that I have carried out.</p>
       <div className="projects">
-      {projects.map(project => (
+      {reversedProjects.map(project => (
         
-          <div key={project.id} className="project">
-            <Link href={`projects/${project.id}`}>
-            <Image src={project.images[0]} alt={project.title} width={1000} height={500} />
+          <div key={project.id} >
+            <Link className="project" href={`projects/${project.id}`}>
+            <Image className="projectImage" src={project.images[0]} alt={project.title} width={1000} height={500} />
             <h3>{project.title}</h3>
             <p>{project.description}</p>
         </Link>
